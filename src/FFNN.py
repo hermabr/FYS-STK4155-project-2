ea46633efd305685f6d763dc03ef41df94ad8bd3
fmwestby <fmwestby@student.matnat.uio.no>
2021-10-27 10:41:17 +0200

    added TODOs

--- src/FFNN.py
+++ src/FFNN.py
@@ -1,11 +1,16 @@
 import numpy as np
+import linear_regression_models
 
-# copied from lectures
-class NeuralNetwork:
+##TODO: make docstrings
+
+# copied from lectures    
+    
+class NeuralNetwork(linear_regression_models): #<---- this may need to change for LogReg
     def __init__(
             self,
             X_data,
             Y_data,
+            activation='none', 
             n_hidden_neurons=50,
             n_categories=10,
             epochs=10,
@@ -15,7 +20,9 @@ class NeuralNetwork:
 
         self.X_data_full = X_data
         self.Y_data_full = Y_data
-
+        
+        self.activation = activation # none: regression task, sigmoid: binary classification, softmax: multi-class classification
+        
         self.n_inputs = X_data.shape[0]
         self.n_features = X_data.shape[1]
         self.n_hidden_neurons = n_hidden_neurons
@@ -38,33 +45,54 @@ class NeuralNetwork:
 
     def feed_forward(self):
         # feed-forward for training
-        self.z_h = np.matmul(self.X_data, self.hidden_weights) + self.hidden_bias
-        self.a_h = sigmoid(self.z_h)
+        self.z_h = self.X_data @ self.hidden_weights + self.hidden_bias
+        
+        if self.activation == 'sigmoid':
+            self.a_h = sigmoid(self.z_h)
+        elif self.activation == 'softmax':
+            pass #TODO add softmax(?)
+        elif self.activation == 'relu':
+            pass #TODO add relu
+        elif self.activation == 'leakyrelu':
+            pass #TODO add leaky relu
+        else: # == 'none'
+            self.a_h = self.z_h
 
-        self.z_o = np.matmul(self.a_h, self.output_weights) + self.output_bias
+        self.z_o = self.a_h @ self.output_weights + self.output_bias
 
         exp_term = np.exp(self.z_o)
         self.probabilities = exp_term / np.sum(exp_term, axis=1, keepdims=True)
 
     def feed_forward_out(self, X):
         # feed-forward for output
-        z_h = np.matmul(X, self.hidden_weights) + self.hidden_bias
-        a_h = sigmoid(z_h)
+        z_h = X @ self.hidden_weights + self.hidden_bias
+        
+        if self.activation == 'sigmoid':
+            a_h = sigmoid(z_h)
+        elif self.activation == 'softmax':
+            pass #TODO add softmax(?)
+        elif self.activation == 'relu':
+            pass #TODO add relu
+        elif self.activation == 'leakyrelu':
+            pass #TODO add leaky relu
+        else: # == 'none'
+            a_h = z_h
 
-        z_o = np.matmul(a_h, self.output_weights) + self.output_bias
+        z_o = a_h @ self.output_weights + self.output_bias
         
         exp_term = np.exp(z_o)
         probabilities = exp_term / np.sum(exp_term, axis=1, keepdims=True)
         return probabilities
 
     def backpropagation(self):
+        # Do we need more here? - we will find out when testing :)
         error_output = self.probabilities - self.Y_data
-        error_hidden = np.matmul(error_output, self.output_weights.T) * self.a_h * (1 - self.a_h)
+        error_hidden = error_output @ self.output_weights.T * self.a_h * (1 - self.a_h)
 
-        self.output_weights_gradient = np.matmul(self.a_h.T, error_output)
+        self.output_weights_gradient = self.a_h.T @ error_output 
         self.output_bias_gradient = np.sum(error_output, axis=0)
 
-        self.hidden_weights_gradient = np.matmul(self.X_data.T, error_hidden)
+        self.hidden_weights_gradient = self.X_data.T @ error_hidden
         self.hidden_bias_gradient = np.sum(error_hidden, axis=0)
 
         if self.lmbd > 0.0:
@@ -100,3 +128,50 @@ class NeuralNetwork:
 
                 self.feed_forward()
                 self.backpropagation()
+
+
+#TODO test for the Franke data set
+
+#TODO find optimal params with heatmap(?)
+
+
+
+
+
+
+
+###notes: 
+
+#For reg fit:
+    #1. change loss to MSE
+    #2. linear output layer                   
+#1 Collect and pre-process data
+#1a create designmatrix X, where each row represents an input and each column represents a feature 
+#1b create label/target vector Y (int) (here we store the correct answers)
+#1c train, test, split dataset
+
+#2 Define model and architecture
+#2a one-against-all strategy (?)
+#2b Sigmoid function (and ReLU?)
+#2c output layer size == # categories
+#2d Normal/uniform dist weights
+#2e bias
+#2f feed forward, softmax
+
+#3 Choose cost function and optimizer
+#3a cost func
+#3b loss func
+#3c cross-entropy vs one-hot vector
+#3d optimizing cost func with gradient descent
+#3e average over a minibatch
+#3f regulate weights with L2-norm
+#3g backpropagation - calc gradient
+
+#4 Train the model
+#4a grid-search - test different hyperparameters separated by orders of magnitude
+
+#5 Evaluate model performance on test data
+#5a accuracy
+
+#6 Adjust hyperparameters (if necessary, network architecture)
+#6a visualize with heatmap
