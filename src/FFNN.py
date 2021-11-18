e6fc7a860f5d451c10044165a89194fc3c1833e3
Herman Brunborg <herman@brunborg.com>
2021-11-18 14:57:35 +0100

    Minor changes

--- src/FFNN.py
+++ src/FFNN.py
@@ -9,15 +9,17 @@ class FFNN:
     def __init__(
         self,
         n_inputs,
-        hidden_sizes=[2],
+        hidden_sizes,
         n_categories=1,
         hidden_layers=SigmoidLayer,
         final_layer=LinearLayer,
+        classification=False,
     ):
         self.n_inputs = n_inputs
         self.n_categories = n_categories
         self.n_hidden_layers = len(hidden_sizes)
-        self.sizes = [self.n_inputs] + hidden_sizes + [self.n_categories]
+        self.classification = classification
+        self.sizes = [self.n_inputs] + list(hidden_sizes) + [self.n_categories]
         self.layers = []
 
         self.layers.append(LinearLayer(1, 1))
@@ -25,16 +27,13 @@ class FFNN:
             if i != len(self.sizes) - 1:
                 self.layers.append(hidden_layers(self.sizes[i], self.sizes[i + 1]))
             else:
-                self.layers.append(final_layer(1, 1))
+                self.layers.append(final_layer(n_categories, n_categories))
 
     def forward_pass(self, x):
         self.layers[0].output = x.reshape(1, -1)
         for i in range(self.n_hidden_layers + 1):
             self.layers[i + 1].forward(self.layers[i].output)
         return self.layers[-1].output
-
-    def grad_sigmoid(self, x):
-        return x * (1 - x)
 
     def backward(self, y, learning_rate):
         L = self.n_hidden_layers + 1
@@ -57,6 +56,8 @@ class FFNN:
         for x in X:
             y_pred = self.forward_pass(x)
             Y_pred.append(y_pred)
+        if self.classification:
+            return np.array(Y_pred).squeeze() > 0.5
         return np.array(Y_pred).squeeze()
 
 
@@ -65,7 +66,12 @@ if __name__ == "__main__":
     data = BreastCancerData(test_size=0.2)
     #  data = FrankeData(20, 1, test_size=0.2)
 
-    net = FFNN(data.X_train.shape[1], [10, 20, 4], final_layer=SigmoidLayer)
+    net = FFNN(
+        data.X_train.shape[1],
+        (10, 20, 4),
+        final_layer=SigmoidLayer,
+        classification=True,
+    )
 
     net.fit(
         data.X_train,
@@ -77,13 +83,13 @@ if __name__ == "__main__":
 
     z_tilde = net.predict(data.X_test)
 
-    mse = np.mean((data.z_test - z_tilde) ** 2)
-    print("BreastCancerData")
-    print(f"MSE: {mse}")
+    #  mse = np.mean((data.z_test - z_tilde) ** 2)
+    #  print("BreastCancerData")
+    #  print(f"MSE: {mse}")
 
     #  print(z_tilde)
-    predict_positive = z_tilde > 0.5
-    correct = predict_positive == data.z_test
+    #  predict_positive = z_tilde > 0.5
+    correct = z_tilde == data.z_test
     print(correct)
     accuracy = np.sum(correct) / len(correct)
     print(accuracy)
